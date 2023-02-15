ddc373ea4d8c886dd45c72445eca931a5b8318a1
Furkan Demir <furkan.vty0@gmail.com>
2023-02-15 15:57:18 +0300

    .

--- Assets/InputHandler.cs
+++ Assets/InputHandler.cs
@@ -8,6 +8,10 @@ public class InputHandler : MonoBehaviour
     public GameObject actor;
     Animator anim;
     Command keyQ, keyW, keyE,upArrow;//command sınıfı türünde verilerimizi oluşturuyoruz.bu veri yapısı miras verdiği sınıfları tür olarak tutabilir.
+    List<Command> oldCommand = new List<Command>();
+    Coroutine replayCoroutine;
+    bool shouldStartReplay;
+    bool isReplaying;
     void Start()
     {
         keyQ = new PerformJump();
@@ -21,21 +25,58 @@ public class InputHandler : MonoBehaviour
    
     void Update()
     {
+        if (!isReplaying)
+            HandleInput();
+        StartReplay();
+        
+    }
+    void HandleInput()
+    {
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            keyQ.Execute(anim);            
+            keyQ.Execute(anim);
+            oldCommand.Add(keyQ);
         }
         else if (Input.GetKeyDown(KeyCode.W))
         {
             keyW.Execute(anim);
+            oldCommand.Add(keyW);
         }
         else if (Input.GetKeyDown(KeyCode.E))
         {
             keyE.Execute(anim);
+            oldCommand.Add(keyE);
         }
-        else if (Input.GetKey(KeyCode.UpArrow))
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             upArrow.Execute(anim);
+            oldCommand.Add(upArrow);
         }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            shouldStartReplay = true;
+        }
+    }
+    void StartReplay()
+    {
+        if (shouldStartReplay&&oldCommand.Count>0)
+        {
+            shouldStartReplay = false;
+            if (replayCoroutine!=null)
+            {
+                StopCoroutine(replayCoroutine);
+            }
+            replayCoroutine = StartCoroutine(ReplayCommands());
+        }
+    }
+    IEnumerator ReplayCommands()
+    {
+        isReplaying = true;
+        for (int i = 0; i < oldCommand.Count; i++)
+        {
+            oldCommand[i].Execute(anim);
+            yield return new WaitForSeconds(1f);
+        }
+        isReplaying = true;
     }
 }
